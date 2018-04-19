5bd0b0b8ac9f8f8c9939a23d6671df652a2e285b
colewrus <wrampelc@gmail.com>
2018-04-18 23:12:23 -0400

    sound manager

---
+++ Assets/scripts/SoundGM.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundGM : MonoBehaviour {
+
+    public AudioSource mySource;
+
+
+	// Use this for initialization
+	void Start () {
+        mySource = this.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void playSound(AudioClip clip)
+    {
+        AudioSource.
+    }
+}
