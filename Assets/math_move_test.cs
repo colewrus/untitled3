ced3292699d2737e28a7b3ba6498992e5559473c
coleTag <cole@taglabs.org>
2019-06-04 12:56:55 -0400

    added math to the fall calculation

--- Assets/math_move_test.cs
+++ Assets/math_move_test.cs
@@ -16,6 +16,8 @@ public class math_move_test : MonoBehaviour
     public bool jumping, onPlatform;
     public float groundMeasure = 1.0f;
     public float fallSpeed;
+    private float fallRate, fallTick;
+  
 
     // Start is called before the first frame update
     void Start()
@@ -59,22 +61,24 @@ public class math_move_test : MonoBehaviour
 
             if (Xtime < 1)
             {
+//rising animation
                 Xtime += Time.deltaTime;
                 Debug.Log(Xtime);
                 //pos += dir * Time.deltaTime * moveSpeed;
 
-                pos = new Vector3(0, Mathf.Log(Xtime/frequency)+magnitude);
+                //pos = new Vector3(0, Mathf.Clamp(Mathf.Log(Mathf.Pow(Xtime, 5) * 5+2), 0, 12), 0);
+
+                pos = new Vector3(0, 2+Mathf.Clamp(Mathf.Sin((frequency * Xtime) * magnitude), 0, 10));
                // pos = new Vector3(0, (3*Mathf.Sin(frequency * Xtime) * magnitude), 0);
                 transform.Translate(pos*Time.deltaTime);
                 //transform.position = pos;
                 //pos + axis * Mathf.Sin(Time.time * frequency) * magnitude;
-
-
             }
             else
             {
                 jumping = false;
                 onPlatform = false;
+ //can set to a falling animation here
             }
 
         }
@@ -111,7 +115,12 @@ public class math_move_test : MonoBehaviour
     {
         if (!jumping && !onPlatform)
         {
-            transform.Translate((Vector2.down * fallSpeed) * Time.deltaTime);
+            //fall speed should speed up to a maximum
+            fallTick += Time.deltaTime;
+          
+            fallRate = Mathf.Clamp(Mathf.Pow(Mathf.Log(fallTick), Mathf.PI), 0, 12);
+            Debug.Log(fallTick);
+            transform.Translate((Vector2.down * fallRate)*Time.deltaTime);
             //transform.position += (Vector3.up * -fallSpeed) * Time.deltaTime;
         }
     }
